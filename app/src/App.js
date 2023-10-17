2c26bddbf65d95c5814a28fa64fe89355441178b
Keval Solanki <Keval.Solanki@adform.com>
2023-10-17 12:54:41 +0530

    Without dist changes

--- app/src/App.js
+++ app/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import {lib1Util1Func1} from 'lib-1/dist'
-import {lib2Util1Func1, lib2Util2Func4} from 'lib-2/dist'
+import {lib1Util1Func1} from 'lib-1'
+import {lib2Util1Func1, lib2Util2Func4} from 'lib-2'
 
 const App = () => {
     lib1Util1Func1()
--- lib-2/util-2/index.js
+++ lib-2/util-2/index.js
@@ -1,6 +1,6 @@
 
-// import { lib1Util1Func1 } from 'lib-1'
-import { lib1Util1Func1 } from 'lib-1/dist'
+import { lib1Util1Func1 } from 'lib-1'
+// import { lib1Util1Func1 } from 'lib-1/dist'
 
 export const lib2Util2Func1 = () => console.log('print-lib2Util2Func1')
 export const lib2Util2Func2 = () => console.log('print-lib2Util2Func2')
